05f55774228ebd0307a0543d8098099d3a02a895
gaurav <gauravuike236@gmail.com>
2024-08-23 18:52:33 +0530

    update method added

--- src/main/java/com/tcs/Adminlogin/controller/Admincontroller.java
+++ src/main/java/com/tcs/Adminlogin/controller/Admincontroller.java
@@ -3,9 +3,11 @@ package com.tcs.Adminlogin.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,11 +34,37 @@ public class Admincontroller {
 		List<Admin> l = ad.getAdminList();
 		return l;
 	}
-	
+
 	@GetMapping(value = "/adm/{id}")
 	public Admin getDatat(@PathVariable int id) {
-		Admin a=ad.getAdmin(id);
+		Admin a = ad.getAdmin(id);
 		return a;
+	}
+
+	@DeleteMapping(value = "/adm/{id}")
+	public String deleteAdmin(@PathVariable int id) {
+		if (id != 0) {
+			String msg = ad.deleteData(id);
+			return msg;
+		} else {
+			return "please show valid data";
+		}
+	}
+	
+	@PutMapping(value="/adm")
+	public Admin updateData(@RequestBody Admin am){
+		Admin a = ad.getAdmin(am.getId());
+		if(a.getId()==am.getId())
+		{
+			a.setName(am.getName());
+			a.setUser(am.getUser());
+			a.setPassword(am.getPassword());
+			String msg=ad.updateAdmin(a);
+			return a;
+		}else {
+		return null;
+		}
+		
 	}
 
 }
